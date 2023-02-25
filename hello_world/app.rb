a9e8aa37582c8179cc1f218adee112721c7195c5
yoshiki_murakami <eiim.yoshiki@gmail.com>
2023-02-25 12:36:34 +1100

    Change variable name

--- hello_world/app.rb
+++ hello_world/app.rb
@@ -5,9 +5,9 @@ require_relative 'official_line'
 
 def lambda_handler(event:, context:)
   # 天気 City
-  weather = Weather.city_new
-  weather_body = weather.get_weather
-  weather_text = weather.make_text(weather_body)
+  weather_city = Weather.city_new
+  weather_body = weather_city.get_weather
+  weather_text = weather_city.make_text(weather_body)
   OfficialLine.send_weather(weather_text)
 
   # レント クローラー
